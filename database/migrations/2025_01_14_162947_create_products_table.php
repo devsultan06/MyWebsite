f6eda83bd2d930c51b5bf126733bf255ae75b1cf
Sultan A. Dev <sultanabaniks@gmail.com>
2025-01-14 17:46:21 +0100

    Add Product model and migration, update home route view

---
+++ database/migrations/2025_01_14_162947_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id(); // Primary key
+            $table->string('name'); // Product name
+            $table->string('image_path'); // Path to the product image
+            $table->decimal('price', 10, 2); // Product price with precision
+            $table->text('description'); // Product description
+            $table->timestamps(); // Created at and Updated at timestamps
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
