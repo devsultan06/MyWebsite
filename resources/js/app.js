2653c28dd24cc046e2a55302605cae44bddc189a
Sultan A. Dev <sultanabaniks@gmail.com>
2025-01-15 22:40:50 +0100

    Implement welcome page layout, add navigation functionality, and include logo and images

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-import './bootstrap';
+import "./bootstrap";
+// app.js
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menuToggle");
+    const closeMenu = document.getElementById("closeMenu");
+    const navLinks = document.getElementById("navLinks");
+
+    // Open Menu
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.add("active");
+    });
+
+    // Close Menu
+    closeMenu.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sultan</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+
+    <nav class="navbar">
+        <div class="navbar-container">
+            <a href="#" class="logo">
+                <img src="{{ asset('images/Logo.png') }}" alt="Logo">Sultan
+            </a>
+            <button class="menu-toggle" id="menuToggle">
+                <i class="fa-solid fa-bars"></i>
+            </button>
+            <ul class="nav-links" id="navLinks">
+
+                <li><a href="#home" class="active"><span>#</span>home</a></li>
+                <li><a href="#about-me"><span>#</span>about-me</a></li>
+                <li><a href="#products"><span>#</span>products</a></li>
+                <li><a href="#cart"><span>#</span>add-to-cart</a></li>
+            </ul>
+            <button class="close-menu" id="closeMenu">
+                <i class="fa-solid fa-times"></i>
+            </button>
+        </div>
+    </nav>
+
+    <section class="hero">
+        <div class="hero-content">
+            <div class="text-section">
+                <h1>
+                    Sultan is a <span class="highlight">web designer</span> and
+                    <span class="highlight">front-end developer</span>
+                </h1>
+                <p class="description">
+                    He crafts responsive websites where technologies meet creativity
+                </p>
+                <button class="contact-btn">Contact me!!</button>
+            </div>
+            <div class="image-section">
+                <img src="{{ asset('images/pp.png') }}" alt="Profile-Picture" class="profile-pic">
+            </div>
+        </div>
+    </section>
+
+    <script
+        src="https://kit.fontawesome.com/b13eb6739b.js"
+        crossorigin="anonymous">
+    </script>
+    @vite('resources/js/app.js')
+</body>
+
+</html>
